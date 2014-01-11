9ee729e02ab42d86acc9dd2a2ffb6deffa68a1a6
michaelcurry <michael.ian.curry@gmail.com>
2014-01-11 02:41:50 -0800

    *1000 on time stamps

--- app/views/default/product.blade.php
+++ app/views/default/product.blade.php
@@ -19,21 +19,20 @@
 		nv.addGraph(function() {
 			var chart = nv.models.lineWithFocusChart();
 
-			chart.lines.forceY([ 0 ]);  // main chart
-			chart.lines2.forceY([ 0 ]); // view finder chart
+			chart.lines
+				.forceY([ 0 ]);
+			chart.lines2
+				.forceY([ 0 ]);
 
 			chart.xAxis
-				.tickFormat(function(d) { return d3.time.format('%d-%b')(new Date(d),0) });
+				.tickFormat(function(d) { return d3.time.format('%x')(new Date(d)) });
 			chart.x2Axis
-				.tickFormat(function(d) { return d3.time.format('%d-%b')(new Date(d),0) });
+				.tickFormat(function(d) { return d3.time.format('%x')(new Date(d)) });
 
 			chart.yAxis
 				.tickFormat(d3.format(',f'));
 			chart.y2Axis
 				.tickFormat(d3.format(',f'));
-
-				chart.lines.forceY([ 0 ]);  // main chart
-				chart.lines2.forceY([ 0 ]); // view finder chart
 
 			d3.select('#chart svg')
 				.datum(
@@ -43,7 +42,7 @@
 							color : '#2ca02c',
 							values : [
 								@foreach($product->snapshots as $snapshot)
-									{ x:{{ strtotime($snapshot->created_at) }} , y:{{ $snapshot->current_total() }} },
+									{ x:{{ strtotime($snapshot->created_at)*1000 }} , y:{{ $snapshot->current_total() }} },
 								@endforeach
 							]
 						},
@@ -52,7 +51,7 @@
 							color : '#ff7f0e',
 							values : [
 								@foreach($product->snapshots as $snapshot)
-									{ x:{{ strtotime($snapshot->created_at) }} , y:{{ $snapshot->backlog_total() }} },
+									{ x:{{ strtotime($snapshot->created_at)*1000 }} , y:{{ $snapshot->backlog_total() }} },
 								@endforeach
 							]
 						}
